ac648e134367d017bc22c93432aa58a57af9bde6
Yunas Qazi <ganifbros@gmail.com>
2015-01-17 05:17:56 +0500

    Integrated Facebook SDK

---
+++ DwindleDating/ThirdParty/FacebookBridge/FacebookBridge.h
@@ -0,0 +1,14 @@
+//
+//  FacebookBridge.h
+//  DwindleDating
+//
+//  Created by Yunas Qazi on 1/17/15.
+//  Copyright (c) 2015 infinione. All rights reserved.
+//
+
+#ifndef DwindleDating_FacebookBridge_h
+#define DwindleDating_FacebookBridge_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
