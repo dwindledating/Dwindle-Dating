dee905bffb97678095043842971fe3e6284a558d
yunas qazi <ganifbros@gmail.com>
2015-12-23 01:32:20 +0500

    0.5 sec delay added.

--- DwindleDating/DwindleDating/MenuController.swift
+++ DwindleDating/DwindleDating/MenuController.swift
@@ -154,7 +154,11 @@ MFMessageComposeViewControllerDelegate {
         }
         else {
             self.view.userInteractionEnabled = true
-            ProgressHUD.dismiss()
+            let delay = 0.5 * Double(NSEC_PER_SEC)
+            let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+            dispatch_after(time, dispatch_get_main_queue()) {
+                ProgressHUD.dismiss()
+            }
         }
     }
     
